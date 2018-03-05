6fb5bc5fa90398ee19756bafe8f0afca9cdaa47b
Patric Vormstein <vormstein@med.uni-frankfurt.de>
2018-03-05 09:17:38 +0100

    Detailed remove command

--- src/HostDoc.ConsoleApp/Commands/RemoveCommand.cs
+++ src/HostDoc.ConsoleApp/Commands/RemoveCommand.cs
@@ -5,7 +5,15 @@ namespace HostDoc.ConsoleApp.Commands
     [Command(Description = "Remove an entry in the host file"), HelpOption]
     public class RemoveCommand
     {
+        [Option(Description = "IP Address of the entry to be removed (optional)", LongName = "ip")]
+        public string IpAddress { get; set; }
 
+        [Option(Description = "Hostname of the entry to be removed (optional)", ShortName = "host")]
+        public string Hostname { get; set; }
+
+        [Option("--all", Description = "Remove all occurences (optional)")]
+        public bool All { get; set; }
+        
         void OnExecute(CommandLineApplication app, IConsole console)
         {
             console.WriteLine("Remove command");
