655812f907f4b23db018b542d1629b2e7298649e
Niels <serious1@tightpowers.com>
2018-05-31 19:07:51 +0200

    MacHostService implementation added.

---
+++ src/HostDoc.Core/Services/MacHostService.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace HostDoc.Core.Services
+{
+    public class MacHostService : HostService
+    {
+
+        public override string GetHostFileLocation()
+        {
+            if (!(HostLocation is null)) 
+                return HostLocation;
+
+            var macdir = Environment.GetEnvironmentVariable("");
+            return macdir;
+        }
+        
+    }
+}
